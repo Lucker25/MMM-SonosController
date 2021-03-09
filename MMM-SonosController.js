73a0b94200d85d9185cd31db8afb90d0ebe6318d
Lucker25 <send2anonym@gmail.com>
2021-03-09 19:56:25 +0100

    Initial Version

---
+++ MMM-SonosController.js
@@ -0,0 +1,235 @@
+/* Magic Mirror
+ * Module: MyFirstTimerModule
+ *
+ * By Nico Arbter
+ * MIT Licensed.
+ * To-Dos: 
+ * jshint esversion: 6
+ */
+
+Module.register("MMM-SonosController",{
+  // Default module config.
+	defaults: {
+    
+  },
+
+	// Define styles.
+	getStyles: function() {
+		return ["MMM-SonosController.css"];
+  },
+  	// Override dom generator.
+	getDom: function() {
+    let that = this; 
+    var wrapper = document.createElement("div");
+    wrapper.className = "sonosModuleDiv";
+    wrapper.id = "SonosControllerWrapper";
+    document.addEventListener("keydown", function (event) {
+      if (event.key == "MediaPlayPause"){
+        that.sendSocketNotification("SONOS_TOGGLE_PLAY", "")
+
+      }
+      else if (event.key == "MediaTrackNext"){
+        that.sendSocketNotification("SONOS_NEXT_SONG", "")
+
+      }
+      else if (event.key == "MediaTrackPrevious"){
+        that.sendSocketNotification("SONOS_PREVIOUS_SONG", "")
+      }
+    });
+
+    let infoDiv = document.createElement("div")
+    infoDiv.id = "infoDiv"
+    infoDiv.className = "infoDiv"; 
+      //adding the cover   
+      let cover = document.createElement("img"); 
+      cover.id = "cover"
+      cover.className = "cover"
+
+      infoDiv.appendChild(cover); 
+
+      let titleDiv = document.createElement("div")
+      titleDiv.id = "titleDiv"
+      titleDiv.className = "titleDiv"; 
+      //adding the artist
+      let artist = document.createElement("span")
+      artist.className = "title"
+      artist.id = "artist"
+      artist.innerHTML = "Artist"; 
+      titleDiv.appendChild(artist); 
+      // adding the title 
+      let title = document.createElement("span")
+      title.className = "title"
+      title.id = "title"
+      title.innerHTML = "Here is the title"; 
+      titleDiv.appendChild(title); 
+
+
+
+    infoDiv.appendChild(titleDiv)
+
+
+    wrapper.appendChild(infoDiv); 
+    let buttonDiv = document.createElement("div")
+    buttonDiv.id = "buttonDiv"
+    buttonDiv.className = "buttonDiv"; 
+      //adding the backbutton
+      var backButton = document.createElement("button");
+      backButton.id = "backButton";
+      backButton.className = "backButton btnClass"; 
+      
+      backButton.addEventListener("click", function(e){
+        console.log("click back")
+        that.sendSocketNotification("SONOS_PREVIOUS_SONG", "")
+      });
+      var image = document.createElement("i");    
+      backButton.innerHTML = ""; 
+      image.className = "fas fa-backward";
+      backButton.appendChild(image); 
+      buttonDiv.appendChild(backButton);
+      
+      // adding the play/pause button 
+      var playButton = document.createElement("button");
+      playButton.id = "playButton";
+      playButton.className = "playButton btnClass"; 
+      
+      playButton.addEventListener("click", function(e){
+        console.log("click play")
+        that.sendSocketNotification("SONOS_TOGGLE_PLAY", "")
+      });
+      var image = document.createElement("i");    
+      playButton.innerHTML = ""; 
+      image.className = "fas fa-play";
+      playButton.appendChild(image); 
+      buttonDiv.appendChild(playButton);
+
+      //adding the nextbutton
+      var nextButton = document.createElement("button");
+      nextButton.id = "nextButton";
+      nextButton.className = "nextButton btnClass"; 
+      
+      nextButton.addEventListener("click", function(e){
+        console.log("click next")
+        that.sendSocketNotification("SONOS_NEXT_SONG", "")
+      });
+      var image = document.createElement("i");    
+      nextButton.innerHTML = ""; 
+      image.className = "fas fa-forward";
+      nextButton.appendChild(image); 
+      buttonDiv.appendChild(nextButton);
+
+    wrapper.appendChild(buttonDiv)
+
+    let volumeSliderContainer = document.createElement("div"); 
+    volumeSliderContainer.className = "sliderDiv"
+      let volumeSlider = document.createElement("input"); 
+      volumeSlider.type = "range"
+      volumeSlider.min = "1"
+      volumeSlider.max = "100"
+      volumeSlider.className = "volumeSlider slider"
+      volumeSlider.id = "volumeSlider"
+      volumeSlider.oninput = function() {
+        that.sendSocketNotification("SET_SONOS_VOLUME", {volume: this.value})
+      }
+    volumeSliderContainer.appendChild(volumeSlider); 
+    wrapper.appendChild(volumeSliderContainer)
+    
+    console.log("wrapper", wrapper)
+		return wrapper;
+  },
+  start: function() {
+    let that = this; 
+    Log.info("Starting module: " + this.name);  
+    this.sendSocketNotification("SONOS_START", "") 
+
+  },
+  sonos: {
+    zone:"",
+
+  },
+  socketNotificationReceived: function (notification, payload) {
+    console.log(notification, payload); 
+    switch(notification){
+      case 'SET_SONOS_CURRENT_TRACK': 
+        document.getElementById("title").innerHTML = payload.track.title; 
+        if (payload.track.albumArtURI != null){
+          document.getElementById("cover").style.display = "block";
+           document.getElementById("cover").src = payload.track.albumArtURI; 
+        }
+        else{
+          document.getElementById("cover").style.display = "none";
+        }
+
+        if (payload.track.duration == 0){
+          document.getElementById("backButton").style.display = "none"; 
+          document.getElementById("nextButton").style.display = "none"; 
+        }
+        else {
+          document.getElementById("backButton").style.display = ""; 
+          document.getElementById("nextButton").style.display = ""; 
+        }
+        document.getElementById("artist").innerHTML = payload.track.artist; 
+        break; 
+      case 'SET_SONOS_PLAY_STATE': 
+          this.setState(payload.state); 
+        break; 
+      case 'SET_SONOS_VOLUME': 
+        console.log("setvolume")
+        document.getElementById("volumeSlider").value = payload.volume; 
+        break; 
+      case 'SET_SONOS_FAVORITES': 
+        this.createList(payload.items);
+        break; 
+      default: 
+
+      break; 
+    }
+  },
+  favListDiv: null, 
+  createList: function(items){
+    let that = this; 
+    if (this.favListDiv == null){
+      this.favListDiv = document.createElement("div")
+      this.favListDiv.className = "favoritesListDiv"
+    }
+    else{
+      this.favListDiv.childNodes.forEach(item =>{
+        console.log(item)
+        document.getElementById(this.favListDiv.id).removeChild(item)
+      })
+    }
+    items.sort((a,b) => a.title > b.title)
+    items.map(item =>{
+      console.log(item)
+      let span = document.createElement("span")
+      span.innerHTML = item.title; 
+      span.className = "favoriteListElement"
+      span.addEventListener("click", function(e){
+        console.log(item.title)
+        that.sendSocketNotification('SET_SONOS_URI', item)
+      })
+      this.favListDiv.appendChild(span)  
+    }); 
+    
+    document.getElementById("SonosControllerWrapper").appendChild(this.favListDiv)
+  },
+  setState: function(state){
+    let playButton = document.getElementById("playButton")
+    
+    var image = document.createElement("i");    
+    playButton.innerHTML = ""; 
+
+    if (state == "playing"){
+      image.className = "fas fa-pause";
+    }
+    else if(state == "paused" || state == "stopped"){
+      image.className = "fas fa-play";
+    }
+    else{
+      image.className = "fas fa-hourglass-start";
+    }
+    playButton.appendChild(image); 
+  }
+  
+  
+});
+ 
---
+++ node_helper.js
@@ -0,0 +1,205 @@
+const NodeHelper = require('node_helper');
+const {AsyncDeviceDiscovery, Listener: listener} = require('../../node_modules/sonos');
+const Sonos  = require('../../node_modules/sonos')
+const Helpers = require('../../node_modules/sonos/lib/helpers')
+
+
+module.exports = NodeHelper.create({
+
+    discovery: null,
+    asyncDevice: null,
+    sonos: null, 
+    library: null,
+
+    init: function() {
+        this.discovery = new AsyncDeviceDiscovery();
+
+    },
+
+    stop: function() {
+        if (listener.isListening()) {
+            listener.stopListener().then(() => {
+                console.debug('Stopped all listeners to Sonos devices');
+            }).catch(error => {
+                console.error(`Failed to stop listeners to Sonos devices, connections might be dangling: ${error.message}`);
+            });
+        }
+    },
+
+    socketNotificationReceived: function (id, payload) {
+      let sonos = this.sonos; 
+          switch (id) {
+            case 'SONOS_START':
+              this.discoverGroups();
+              
+              break;
+            case 'SONOS_TOGGLE_PLAY': 
+              this.sendSocketNotification("SONOS", sonos); 
+              sonos.togglePlayback(); 
+              break; 
+            case 'SONOS_NEXT_SONG': 
+              if (sonos != null)    
+              sonos.next(); 
+              break; 
+            case 'SONOS_PREVIOUS_SONG': 
+              if (sonos != null)    
+                sonos.previous(); 
+              break; 
+            case 'SET_SONOS_VOLUME': 
+              if (sonos != null)    
+              sonos.setVolume(payload.volume);
+            break; 
+            case 'SET_SONOS_URI': 
+              if (sonos != null) {
+               
+              if (payload.uri.includes("spotify"))
+                payload.uri = fixSpotify(payload.uri)
+              
+              sonos.flush().then(result => {
+                sonos.queue(payload.uri).then(result => {
+                  console.log("result", result)
+                  sonos.selectQueue();
+                  sonos.play();
+                }).catch(error => {console.log(error)});
+              });
+            }
+              break;
+            default:
+              console.info(`Notification with ID "${id}" unsupported. Ignoring...`);
+              break;
+            function fixSpotify(uri){
+              uri = uri.slice(uri.indexOf("spotify"))
+              uri = uri.slice(0, (uri.indexOf("?") - uri.length)); 
+              uri = uri.split("%3a").join(":")
+              console.log("fixed", uri)
+              return uri
+
+
+            }
+        }
+    },
+
+    discoverGroups: function(attempts = 0) {
+        let that = this; 
+        if (!this.asyncDevice) {
+            this.asyncDevice = this.discovery.discover().then(device => {
+                listener.on('ZonesChanged', () => {
+                    console.log(`Zones have changed. Rediscovering all groups ...`);
+                    this.discoverGroups();
+                });
+                return listener.subscribeTo(device).then(() => {
+                    return device;
+                })
+            })
+        }
+        this.asyncDevice.then(device => {
+            return device.getAllGroups();
+        }).then(groups => {
+            this.setGroups(groups);
+            groups.forEach(group => {
+              that.sonos = group.CoordinatorDevice(); //this only works with one Group
+              that.sonos.setSpotifyRegion(Sonos.SpotifyRegion.EU); 
+            }); 
+            this.getLibrary(groups);
+        }).catch(error => {
+            attempts++;
+            const timeout = Math.min(Math.pow(attempts, 2), 30);
+            console.error(`Failed to get groups: ${error.message}. Retrying in ${timeout} seconds ...`);
+            that.sonos = null; // only one group
+            if (listener.isListening()) {
+                listener.stopListener().then(() => {
+                    console.debug('Stopped all listeners to Sonos devices');
+                }).catch(error => {
+                    console.error(`Failed to stop listeners to Sonos devices, connections might be dangling: ${error.message}`);
+                });
+            }
+            this.asyncDevice = null;
+            setTimeout(() => {
+                this.discoverGroups(attempts);
+            }, timeout * 1000);
+        });
+    },
+
+    getLibrary(groups){
+      Promise.all(groups.map(group => {
+        const sonos = group.CoordinatorDevice();
+            return Promise.all([
+                sonos.getFavorites()
+            ]).then(favs =>{
+              this.sendSocketNotification("SET_SONOS_FAVORITES", favs[0])
+            });
+      })); 
+    },
+
+    setGroups(groups) {
+        Promise.all(groups.map(group => {
+            const sonos = group.CoordinatorDevice();
+            return Promise.all([
+                sonos.currentTrack(),
+                sonos.getCurrentState(),
+                sonos.getVolume(),
+                sonos.getMuted()
+            ]).then(data => {
+                return {
+                    group,
+                    track: data[0],
+                    state: data[1],
+                    volume: data[2],
+                    isMuted: data[3],
+                };
+            });
+        })).then(items => {
+            this.sendSocketNotification('SET_SONOS_GROUPS', items.reduce((map, item) => {
+                map[item.group.ID] = item;
+                return map;
+            }, {}));
+            return items;
+        }).then(groups => {
+            this.setListeners(groups.map(item => item.group));
+        });
+    },
+    
+    setListeners: function(groups) {
+        groups.forEach(group => {
+            console.log(`Registering listeners for group "${group.Name}" (host "${group.host}")`);
+
+            const sonos = group.CoordinatorDevice();
+
+            sonos.on('Mute', isMuted => {
+                console.log('This speaker is %s.', isMuted ? 'muted' : 'unmuted')
+            });
+
+            sonos.on('CurrentTrack', track => {
+                console.log(`[Group ${group.Name} - ${group.host}] Track changed to "${track.title}" by "${track.artist}"`);
+                this.sendSocketNotification('SET_SONOS_CURRENT_TRACK', {
+                    group,
+                    track
+                });
+            });
+
+            sonos.on('Volume', volume => {
+                console.log(`[Group ${group.Name} - ${group.host}] Volume changed to "${volume}"`);
+                this.sendSocketNotification('SET_SONOS_VOLUME', {
+                    group,
+                    volume
+                });
+            });
+
+            sonos.on('Muted', isMuted => {
+                console.log(`[Group ${group.Name} - ${group.host}] Group is ${isMuted ? 'muted' : 'unmuted'}`);
+                this.sendSocketNotification('SET_SONOS_MUTE', {
+                    group,
+                    isMuted
+                });
+            });
+
+            sonos.on('PlayState', state => {
+                console.log(`[Group ${group.Name} - ${group.host}] Play state change to "${state}"`);
+                this.sendSocketNotification('SET_SONOS_PLAY_STATE', {
+                    group,
+                    state
+                });
+            });
+        });
+    }
+});
